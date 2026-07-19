48b48d2a4c01bca805aafa045ed2786fb95a9141
Chamballi Jayant Naidu <155427377+Jayantnaidu1106@users.noreply.github.com>
2026-07-19 22:59:02 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.83%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string ans="";
+     vector<int> al(26,-1);
+     vector<bool> vis(26,false);
+     int n = s.size();
+     for(int i =0;i<n;i++){
+        al[s[i] - 'a'] = i;
+     }
+     for(int i = 0;i<n;i++){
+        if(ans.size() == 0 || !vis[s[i] - 'a'] && ans.back() < s[i]){
+            ans.push_back(s[i]);
+            vis[s[i] - 'a'] = true;
+            continue;
+        }
+        while(ans.size()>0 && ans.back()>s[i] &&!vis[s[i]-'a'] && al[ans.back() - 'a']>i){
+            vis[ans.back() - 'a'] = false;
+            ans.pop_back();
+        }
+      if(!vis[s[i]-'a']){
+
+        ans.push_back(s[i]);
+        vis[s[i]-'a'] = true;
+      }
+     }
+     return ans;
+    }
+};
